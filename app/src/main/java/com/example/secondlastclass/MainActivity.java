02b9478701445150858ebbdd857c5caf4a7ab6e5
1794977 <amandeep_suggal@yahoo.com>
2020-03-12 16:00:53 -0400

    hariii................

--- app/src/main/java/com/example/secondlastclass/MainActivity.java
+++ app/src/main/java/com/example/secondlastclass/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.widget.TooltipCompat;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -24,6 +28,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupNavigation();
     }
 
     public void setupNavigation(){
@@ -35,12 +41,59 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
         drawerLayout = findViewById(R.id.drawer_layout);
-
         navigationView= findViewById((R.id.navigation_view));
+        navController= Navigation.findNavController(this,R.id.nav_host_fragment);
+
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout);
+        NavigationUI.setupWithNavController(navigationView,navController);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
+
+    @Override
+    public void onBackPressed() {
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer((GravityCompat.START));
+
+        }else
+        {
+        super.onBackPressed(); }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+
+        return NavigationUI.navigateUp(Navigation.findNavController(this,R.id.nav_host_fragment),drawerLayout);
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
+
+        menuItem.setCheckable(true);
+        drawerLayout.closeDrawers();
+        int id = menuItem.getItemId();
+
+        switch (id)
+        {
+            case R.id.firstfragment:
+
+                Toast.makeText(getApplicationContext(),"First Clicked!", Toast.LENGTH_SHORT).show();
+                navController.navigate(R.id.firstfragment);
+
+            case R.id.secondFragment:
+
+                Toast.makeText(getApplicationContext(),"Second Clicked!", Toast.LENGTH_SHORT).show();
+                navController.navigate(R.id.secondFragment);
+
+            case R.id.thirdFragment:
+
+                Toast.makeText(getApplicationContext(),"Third Clicked!", Toast.LENGTH_SHORT).show();
+                navController.navigate(R.id.thirdFragment);
+        }
+
+
+        return true;
     }
 }
